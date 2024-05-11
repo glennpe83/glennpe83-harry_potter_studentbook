15ccdaf238d1f6174070c8eb75df24552b8a3cd8
Glenn Pedersen <glenn.pedersen@gokstadakademiet.no>
2024-05-12 00:31:12 +0200

    Stylet containere og studenter på hovedsiden

--- JS/script.js
+++ JS/script.js
@@ -27,17 +27,47 @@ const studentlistContainer = document.querySelector('#studentlistContainer');
 
 //Show students
 const showStudents = () => {
+    
     allStudents.forEach((student) => {
+        if (student.image !== "") {
     const divContainer = document.createElement('div');
     const divStudentContainer = document.createElement('div');
     const studentImage = document.createElement('img');
     const studentName = document.createElement('h3');
     const studentHouse = document.createElement('p');
 
-   
     studentName.innerHTML = student.name;
     studentImage.src = student.image;
     studentHouse.innerHTML = student.house;
+
+    studentImage.alt = student.name;
+    studentImage.title = `Bilde av ${student.name}`;
+
+    //Styling av side via DOM
+    studentlistContainer.style.display = 'flex';
+    studentlistContainer.style.flexFlow = 'row wrap';
+    studentlistContainer.style.gap = '30px';
+    studentlistContainer.style.margin = '30px';
+    studentlistContainer.style.padding = '30px';
+
+    divContainer.style.display = 'flex';
+    divContainer.style.alignItems = 'center';
+    divContainer.style.justifyContent = 'space-evenly';
+    divContainer.style.flexFlow = 'column wrap';
+    divContainer.style.border = '2px black solid';
+    
+
+    divStudentContainer.style.display = 'flex';
+    divStudentContainer.style.justifyContent = 'center';
+    divStudentContainer.style.width = '250px';
+
+    studentImage.style.height = '300px';
+    studentImage.style.width = '250px';
+  
+
+    studentHouse.style.color = 'black';
+    studentHouse.style.fontSize = '1.3rem';
+    studentHouse.style.marginBottom = '10px';
 
     
 
@@ -50,4 +80,4 @@ const showStudents = () => {
     
     
 
-})};
+}})};
