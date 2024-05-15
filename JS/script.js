1248dc8b7821af6063c0d60365ee7eeade00049c
Glenn Pedersen <glenn.pedersen@gokstadakademiet.no>
2024-05-15 14:07:48 +0200

    Stylet ferdig studentcontainere på forside og i overlay

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,4 @@
 //Fetch api
-
 const BASE_URL = "https://hp-api.onrender.com/api/characters/students";
 
 let allStudents;
@@ -52,6 +51,7 @@ const showStudents = () => {
       divContainer.style.justifyContent = "space-evenly";
       divContainer.style.flexFlow = "column wrap";
       divContainer.style.border = "2px black solid";
+      divContainer.style.color = 'white';
 
       divStudentContainer.style.display = "flex";
       divStudentContainer.style.justifyContent = "center";
@@ -60,9 +60,20 @@ const showStudents = () => {
       studentImage.style.height = "300px";
       studentImage.style.width = "250px";
 
-      studentHouse.style.color = "black";
+      studentHouse.style.color = "white";
       studentHouse.style.fontSize = "1.3rem";
       studentHouse.style.marginBottom = "10px";
+
+      if (student.house === 'Gryffindor') {
+        divContainer.style.backgroundColor = '#7F0909';
+      } else if (student.house === 'Ravenclaw') {
+        divContainer.style.backgroundColor = '#0E1A40';
+      } else if (student.house === 'Slytherin') {
+        divContainer.style.backgroundColor = '#1A472A';
+      } else {
+        divContainer.style.backgroundColor = 'black';
+      }
+
 
       divContainer.appendChild(studentName);
       divContainer.appendChild(studentImage);
@@ -83,42 +94,77 @@ const showStudent = (student) => {
   const studentImage = document.createElement("img");
   const studentName = document.createElement("h3");
   const studentHouse = document.createElement("p");
+  const yearOfBirth = document.createElement('p');
+  const ancestry = document.createElement('p');
+
   const buttonContainer = document.createElement('div');
   const closeBtn = document.createElement('button');
-  const saveStudentBtn = document.createElement('button');
+  const addToMyListBtn = document.createElement('button');
+  
 
-  studentName.innerHTML = student.name;
+  overlay.style.display = "flex";
+  overlay.style.alignItems = "center";
+  overlay.style.justifyContent = "space-evenly";
+  overlay.style.flexFlow = "flexrow wrap";
+  overlay.style.borderRadius = '45px';
+
+  
+
+  studentContainer.style.display = 'flex';
+  studentContainer.style.flexDirection = 'column';
+  studentContainer.style.justifyContent = 'center';
+  studentContainer.style.alignItems = 'center';
+
+
+
+  studentName.innerHTML = `Navn: ${student.name}`;
   studentImage.src = student.image;
-  studentHouse.innerHTML = student.house;
+  studentHouse.innerHTML = `Medlem av House ${student.house}`;
+  yearOfBirth.textContent = student.yearOfBirth === null ? 'Ukjent fødselsår' : `Fødselsår: ${student.yearOfBirth}`;
+  ancestry.textContent = student.ancestry === '' ? 'Ukjent herkomst' : student.ancestry;
+
+
+
+  studentName.style.fontSize = '30px';
 
   studentImage.alt = student.name;
   studentImage.title = `Bilde av ${student.name}`;
 
-  studentImage.style.height = "300px";
-  studentImage.style.width = "250px";
+  studentImage.style.height = "400px";
+  studentImage.style.width = "350px";
+  studentImage.style.margin = '30px';
 
   studentHouse.style.color = "black";
-  studentHouse.style.fontSize = "1.3rem";
+  studentHouse.style.fontSize = "1.5rem";
   studentHouse.style.marginBottom = "10px";
 
+  
+
   buttonContainer.style.display = 'flex';
-  buttonContainer.style.justifyContent = 'space-evenly'
+  buttonContainer.style.justifyContent = 'space-evenly';
+  buttonContainer.style.margin = '15px';
  
 
   closeBtn.textContent = 'Lukk vindu';
+  closeBtn.style.fontSize = '15';
   closeBtn.style.padding = '15px';
+  
 
-  saveStudentBtn.textContent = 'Lagre i liste';
-  saveStudentBtn.style.padding = '15px';
+  addToMyListBtn.textContent = 'Lagre i liste';
+  addToMyListBtn.style.fontSize = '15';
+  addToMyListBtn.style.padding = '15px';
 
 
   studentContainer.appendChild(studentName);
   studentContainer.appendChild(studentImage);
   studentContainer.appendChild(studentHouse);
-  studentContainer.appendChild(buttonContainer);
-  buttonContainer.appendChild(closeBtn);
-  buttonContainer.appendChild(saveStudentBtn);
+  studentContainer.appendChild(yearOfBirth);
+  studentContainer.appendChild(ancestry);
   overlay.appendChild(studentContainer);
+  overlay.appendChild(buttonContainer);
+  buttonContainer.appendChild(closeBtn);
+  buttonContainer.appendChild(addToMyListBtn);
+
 
   overlay.style.display = "block";
 
